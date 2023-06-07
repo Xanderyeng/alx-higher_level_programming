1294bd187abf2c04198b27c4699d2f53ec0c6bcc
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-06-06 20:10:19 -0700

    Islower

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
