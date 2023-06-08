8fe2ecbf5bee5ac40165d0c1b8c604bb1542952e
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-06-07 20:02:09 -0700

    Python if else

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end='')
+#!/usr/bin/python3
+for i in range(97, 123):
+    if (i != 101):
+        if (i != 113):
+            print("{:c}".format(i), end="")
