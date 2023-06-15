d74cca74bb3f2a425de4c47d823a1f35e8c4b950
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-06-14 19:44:29 -0700

    Squares

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
+    squares = []
     for row in matrix:
-        new_row = []
-        for value in row:
-            new_row.append(value ** 2)
-        new_matrix.append(new_row)
-    return new_matrix
-
+        squares.append(list(map(lambda x: x**2, row)))
+return squares
