344b3866527b5e4d3a44a9d0015fba4d0b4a0f56
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-07-03 18:42:06 -0700

    Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Create an empty class called Rectangle.
+
+Creation date: July 03, 2023.
+
+Author: Chepkiyeng Alexander.
+"""
+
+
+class Rectangle:
+    """
+    create the new class Rectangle.
+
+    atributes: empty.
+    """
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Create an empty class called Rectangle.
+Creation date: July 03, 2023.
+Author: Chepkiyeng Alexander.
+"""
+
+
+class Rectangle:
+    """
+    create the new class Rectangle.
+    atributes:
+    width: as private instance
+    height: as a private instance
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
