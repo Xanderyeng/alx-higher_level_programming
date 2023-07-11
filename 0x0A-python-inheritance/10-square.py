ec1abbe8ec6c7d5e6bb1ed3a11141ba7cb588275
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-07-11 14:59:47 -0700

    square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """
-Imports class Rectangle
+New subclass square
 """
 
-from rectangle import Rectangle
-
-
-"""
-Defines class Square
-"""
-
+Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
-    """ Initializes class """
+    """
+    inhetrance from Rectangle
+    """
     def __init__(self, size):
         self.integer_validator("size", size)
-        super().__init__(size, size)
         self.__size = size
 
-    """ Public instance method to calculate area """
     def area(self):
-        return self.__size ** 2
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__size) + "/" + str(self.__size)
