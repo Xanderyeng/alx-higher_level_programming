ab617886f60a6da9c98f21368c55eac47b3d1c30
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-08-15 07:59:58 -0700

    Table manipulation using AVG, UPDATE, COUNT, IS NOT NULL and more

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,1 +1,2 @@
+-- Adding a new Row --
 INSERT INTO first_table (id, name) VALUES (89, 'Best School');
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,1 +1,2 @@
+-- Selecting Data from the Table based on a specifc field value --
 SELECT COUNT(*) FROM first_table WHERE id = 89;
