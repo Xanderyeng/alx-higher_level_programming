765c21ccb519c96f24de0b21bb4456f2d89ab322
xanderyeng <alexander.chepkiyeng@gmail.com>
2023-08-16 11:02:14 -0700

    SQL Queries and Join Types

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,14 +1,3 @@
--- script that creates the table force_name --
-SET @db_name = 'your_database_name';
-
--- Create table force_name if it doesn't exist
-CREATE TABLE IF NOT EXISTS `force_name` (
-    `id` INT PRIMARY KEY,
-    `name` VARCHAR(256) NOT NULL
-) ENGINE=InnoDB;
-
--- Use the specified database
-USE @db_name;
-
--- Show table information
-DESCRIBE force_name;
+-- script that creates the table force_name on your MySQL server.
+-- with conditions
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
