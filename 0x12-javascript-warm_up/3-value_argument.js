775685e8820225e69e2e65e41d2705d81af8379d
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2023-09-12 17:34:55 +0300

    Javascript warm-up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
-if (args[0] !== undefined) {
-  console.log(args[0]);
-} else {
-  console.log("No argument");
-}
+if (process.argv[2]) {
+    console.log(process.argv[2]);
+  } else {
+    console.log('No argument');
+  }
+  
