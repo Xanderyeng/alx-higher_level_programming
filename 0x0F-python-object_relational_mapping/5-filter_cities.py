ee1c4174f69020566803d36e786ea83a066aa8dd
xanderyeng <ckalex54@gmail.com>
2023-09-22 22:31:29 +0300

    retrying

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
 """
-Write a script that takes in the name of a state as an argument and
-lists all cities of that state, using the database hbtn_0e_4_usa
+Module for script that takes the name of a state as an argument and lists all
+cities of that state
 """
-import MySQLdb
-import sys
-arg = sys.argv
 
+
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        user=arg[1],
-        passwd=arg[2],
-        db=arg[3],
-        port=3306)
-    state = arg[4]
+    state = sys.argv[4]
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host=host, port=port, user=user,
+                         passwd=passwd, db=database)
     cur = db.cursor()
-    i = 1
 
-    count = cur.execute("SELECT cities.name FROM cities WHERE state_id = ( \
-                        SELECT id \
-                        FROM states \
-                        WHERE name LIKE BINARY %s) \
-                        ORDER BY cities.id ASC", (state, ))
-    names = cur.fetchall()
-    for item in names:
-        print(item[0], end="")
-        if i < count:
-            print(end=", ")
-            i += 1
+    sql_cmd = "SELECT cities.name FROM cities WHERE cities.state_id=(SELECT id\
+              FROM states WHERE name='{}') ORDER BY cities.id".format(state,)
+    cur.execute(sql_cmd)
+    cities = cur.fetchall()
+    separator = ""
+    for city in cities:
+        print(separator, end="")
+        print(city[0], end="")
+        separator = ", "
     print()
     cur.close()
     db.close()
